765d789bd4efa6b45d87a3d012e18e873b744fc3
Don Oerkfitz <don@oerkfitz.com>
2017-12-08 15:03:13 -0600

    initialized eslintrc

--- webview.js
+++ webview.js
@@ -4,7 +4,7 @@ module.exports = (Franz) => {
     function getMessages() {
         let direct = 0;
         let indirect = 0;
-        const FranzData = document.querySelector('#FranzMessages').dataset;
+        const FranzData = document.querySelector("#FranzMessages").dataset;
         if (FranzData) {
             direct = FranzData.direct;
             indirect = FranzData.indirect;
